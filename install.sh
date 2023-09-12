c963ff8d93780c58a8bf68a822b8ef5be8c0bfc3
genomewalker <genomewalker@gmail.com>
2023-09-12 14:14:30 +0200

    Cosmetics

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@ echo "Copying dotfiles ..."
 echo "STEP: copying .gitconfig and .gitignore_global"
 cp -r ./git/.gitconfig ./git/.gitignore_global ~
 
-
 PATH_CS_1="$HOME/.local/share/code-server"
 PATH_CS_2="$HOME/.local/share/code-server/User"
 PATH_VS_1="$HOME/Library/Application Support/Code/User"
@@ -18,9 +17,9 @@ COMMAND_T="cp ./Code/User/tasks.json"
 echo "STEP: copying shell dotfiles e.g., .bashrc, .zshrc"
 echo "Shell is $SHELL"
 
-if [ "$SHELL" == "/bin/bash" ]; then 
+if [ "$SHELL" == "/bin/bash" ]; then
   cp ./shell/bash/.bashrc $HOME/.bashrc
-  cp ./shell/bash/.bash_profile $HOME/.bash_profile  
+  cp ./shell/bash/.bash_profile $HOME/.bash_profile
 elif [ "$SHELL" == "/bin/zsh" ]; then
   cp ./shell/zsh/.zshrc $HOME/.zshrc
 elif [ "$SHELL" == "/usr/local/fish" ]; then
@@ -33,23 +32,23 @@ echo "STEP: copying VS Code-related config files"
 
 if [ -d $PATH_CS_1 ]; then
 
-    echo 'code-server directory exists (i.e., a Coder remote workspace), copying settings.json, keybindings.json, tasks.json' 
-    if [ -d $PATH_CS_2 ]; then        
-        echo "$PATH_CS_2 found"  
-    else
-        echo "$PATH_CS_2 not found, make directory"
-        mkdir "$PATH_CS_1"/User    
-    fi
+  echo 'code-server directory exists (i.e., a Coder remote workspace), copying settings.json, keybindings.json, tasks.json'
+  if [ -d $PATH_CS_2 ]; then
+    echo "$PATH_CS_2 found"
+  else
+    echo "$PATH_CS_2 not found, make directory"
+    mkdir "$PATH_CS_1"/User
+  fi
 
-    $COMMAND_S $PATH_CS_2
-    $COMMAND_K $PATH_CS_2
-    $COMMAND_T $PATH_CS_2
+  $COMMAND_S $PATH_CS_2
+  $COMMAND_K $PATH_CS_2
+  $COMMAND_T $PATH_CS_2
 
 fi
 
 if [ -d "$PATH_VS_1" ]; then
-    echo 'locally-installed VS Code found, copying settings.json, keybindings.json, tasks.json'
-    $COMMAND_S "$PATH_VS_1"
-    $COMMAND_K "$PATH_VS_1"
-    $COMMAND_T "$PATH_VS_1"  
+  echo 'locally-installed VS Code found, copying settings.json, keybindings.json, tasks.json'
+  $COMMAND_S "$PATH_VS_1"
+  $COMMAND_K "$PATH_VS_1"
+  $COMMAND_T "$PATH_VS_1"
 fi
